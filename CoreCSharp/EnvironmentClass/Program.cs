b30cceca5d3bef2b61847994ffba86e4c8a4db25
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-03 12:50:57 +0400

    Getting familiar with System.Environment Class

---
+++ CoreCSharp/EnvironmentClass/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EnvironmentClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowEnvironmentDetails();
+        }
+
+        static void ShowEnvironmentDetails()
+        {
+            Console.WriteLine("Domain Name: {0}", Environment.UserDomainName);
+            Console.WriteLine("Machine Name: {0}", Environment.MachineName);
+            Console.WriteLine("User Name: {0}", Environment.UserName);
+            Console.WriteLine("\nMachines's Drives: ");
+            foreach (string drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive {0}", drive);
+            }
+
+            Console.WriteLine("\nOS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET Core Version: {0}", Environment.Version);
+
+            Console.WriteLine("\nCommand Line: {0}", Environment.CommandLine);
+            Console.WriteLine("\nCurrent Directory: {0}", Environment.CurrentDirectory);
+            Console.WriteLine("\nDesktop Directory: {0}", Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+
+            Console.WriteLine("\nProcess Path: {0}", Environment.ProcessPath);
+            Console.WriteLine("\nMinutes since the machine's start: {0}", Environment.TickCount64/1000/60);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# :blue_book: A collection of C# projects and exercises built while working through Pro C# by Andrew Troelsen. 
+
+This repository contains my personal learning journey through the book. 
+
+## :dart: Goal
+
+To understand C# and .NET fundamentals by reading the book and implementing practical examples, mini-projects, and exercises chapter by chapter. This repo serves as a structured learning log and a reference for future projects.
+
+## :file_folder: Structure
+
+All content is organized under the `ProCSharp` folder. Each chapter or major topic from the book is represented as a separate **Visual Studio solution (.sln)**. These solutions contain one or more projects that demonstrate specific concepts, examples, and exercises.
