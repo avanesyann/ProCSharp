76880376f0751d2fad0460351e06f941c7d63e2b
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-09 20:56:07 +0400

    Complete FrequencyCounter

--- CoreCSharp/FrequencyCounter/Program.cs
+++ CoreCSharp/FrequencyCounter/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace FrequencyCounter
+﻿using System.Text.RegularExpressions;
+
+namespace FrequencyCounter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your sentence: ");
+            string sentence = Console.ReadLine();
+            Dictionary<string, int> occurrences = new Dictionary<string, int>();
 
+            string cleaned = Regex.Replace(sentence, @"[^\w\s]", "").ToLower();
+
+            string[] words = cleaned.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (string word in words)
+            {
+                if (occurrences.ContainsKey(word))
+                    occurrences[word]++;
+                else
+                    occurrences.Add(word, 1);
+            }
+
+            foreach (var wrd in occurrences)
+            {
+                Console.WriteLine($"{wrd.Key}: {wrd.Value}");
+            }
         }
     }
 }
--- CoreCSharp/FrequencyCounter/README.md
+++ CoreCSharp/FrequencyCounter/README.md
@@ -1,3 +1,1 @@
 ## :wrench: Project: Word Frequency Counter
-
-This repository contains my personal learning journey through the book.
