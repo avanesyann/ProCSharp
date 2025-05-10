e4e981b1373c8988c4498708bc39973d2f6ae212
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-10 23:41:17 +0400

    Rock, Paper, Scissors!

--- CoreCSharp/PatternMatching/Program.cs
+++ CoreCSharp/PatternMatching/Program.cs
@@ -22,6 +22,14 @@ namespace PatternMatching
             SwitchPatternMatching(8);
             SwitchPatternMatching(140);
             SwitchPatternMatching(99);
+            SwitchPatternMatching(999999999999L);
+            SwitchPatternMatching(-9.9);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Rock, Paper, Scissors!");
+            Console.WriteLine(RockPaperScissors("rock", "paper"));
+            Console.WriteLine(RockPaperScissors("rock", "scissors"));
 
         }
 
@@ -44,7 +52,7 @@ namespace PatternMatching
                 Console.WriteLine($"{ulongItem} is a ulong.");
             }
         }
-        
+
         static void IfElseUpdated(object value)
         {
             if (value is >= 10 and < 100)
@@ -57,15 +65,18 @@ namespace PatternMatching
             }
         }
 
-        static void SwitchPatternMatching(int value)
+        static void SwitchPatternMatching(object value)
         {
             switch (value)
             {
-                case < 0:
-                    Console.WriteLine($"{value} is negative.");
+                case int i and < 0:
+                    Console.WriteLine($"integer {i} is negative.");
                     break;
-                case < 10:
-                    Console.WriteLine($"{value} is a single-digit number.");
+                case double d and < 0:
+                    Console.WriteLine($"doulbe {d} is negative.");
+                    break;
+                case int i and < 10:
+                    Console.WriteLine($"integer {i} is a single-digit number.");
                     break;
                 case < 100:
                     Console.WriteLine($"{value} is a double-digit number.");
@@ -73,10 +84,27 @@ namespace PatternMatching
                 case > 100 and < 200:
                     Console.WriteLine($"{value} is between 100 and 200");
                     break;
+                case long or ulong:
+                    Console.WriteLine($"{value} is quite long.");
+                    break;
                 default:
                     Console.WriteLine("Unknown");
                     break;
             }
+        }
+
+        static string RockPaperScissors(string first, string second)
+        {
+            return (first, second) switch
+            {
+                ("rock", "paper") => "Paper wins.",
+                ("rock", "scissors") => "Rock wins.",
+                ("paper", "rock") => "Paper wins.",
+                ("paper", "scissors") => "Scissors wins.",
+                ("scissors", "rock") => "Rock wins.",
+                ("scissors", "paper") => "Scissors wins.",
+                (_, _) => "Tie.",
+            };
         }
     }
 }
