de19e32968e4c123641b1de158d111f44d8d7bc6
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-14 23:04:24 +0400

    Understanding Methods

---
+++ CoreCSharp/UnderstandingMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace UnderstandingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddWrapper(15, 25));
+        }
+
+        static int AddWrapper(int x, int y)
+        {
+            return Add();
+
+            int Add() => x + y;
+        }
+    }
+}
