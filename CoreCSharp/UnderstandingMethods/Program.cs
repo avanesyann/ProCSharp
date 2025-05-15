2e0dbffe2b7c4307a306db3915b37b79023d218b
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-15 22:40:33 +0400

    Out parameter.

--- CoreCSharp/UnderstandingMethods/Program.cs
+++ CoreCSharp/UnderstandingMethods/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine(AddWrapper(15, 25));
+            Console.WriteLine(AddUsingOut(15, 35, out int ans));
+            Console.WriteLine(AddUsingOut(15, 15, out _));
+
+            Console.WriteLine(ans);
         }
 
         static int AddWrapper(int x, int y)
@@ -12,6 +16,13 @@
             return Add();
 
             int Add() => x + y;
+        }
+        
+        static int AddUsingOut(int x, int y, out int ans)
+        {
+            ans = x + y;
+
+            return ans * ans;
         }
     }
 }
