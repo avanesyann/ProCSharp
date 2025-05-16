38c32ef352eaccdf9a6ff8a63a4f936b836f001a
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-16 22:40:49 +0400

    Ref parameter.

--- CoreCSharp/UnderstandingMethods/Program.cs
+++ CoreCSharp/UnderstandingMethods/Program.cs
@@ -9,6 +9,18 @@
             Console.WriteLine(AddUsingOut(15, 15, out _));
 
             Console.WriteLine(ans);
+
+            string s1 = "Mama";
+            string s2 = "Mia";
+            int i1 = 12;
+            int i2 = 14;
+
+            Console.WriteLine($"{s1} {s2}");
+            Console.WriteLine($"{i1} {i2}");
+            SwapStrings(ref s1, ref s2);
+            SwapInts(ref i1, ref i2);
+            Console.WriteLine($"{s1} {s2}");
+            Console.WriteLine($"{i1} {i2}");
         }
 
         static int AddWrapper(int x, int y)
@@ -23,6 +35,19 @@
             ans = x + y;
 
             return ans * ans;
+        }
+
+        static void SwapStrings(ref string s1, ref string s2)
+        {
+            string temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+        static void SwapInts(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
         }
     }
 }
