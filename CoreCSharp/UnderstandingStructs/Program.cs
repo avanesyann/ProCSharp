fe7f196818d7678d64332a4bf5641d962b02e524
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-21 23:56:03 +0400

    Understanding Structures

---
+++ CoreCSharp/UnderstandingStructs/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UnderstandingStructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point myPoint;
+            myPoint.X = 349;
+            myPoint.Y = 76;
+            myPoint.Display();
+
+            myPoint.Increment();
+            myPoint.Display();
+        }
+
+
+    }
+    struct Point
+    {
+        public int X;
+        public int Y;
+
+        public void Increment()
+        {
+            X++; Y++;
+        }
+        public void Decrement()
+        {
+            X--; Y--;
+        }
+        public void Display()
+        {
+            Console.WriteLine("X = {0}, Y = {1}", X, Y);
+        }
+    }
+}
