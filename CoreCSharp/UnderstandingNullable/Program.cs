9a36ef0ae4f499fc93d1a2e4fae27a2a2e8ab23e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-05-25 22:41:29 +0400

    Understanding Nullable

---
+++ CoreCSharp/UnderstandingNullable/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace UnderstandingNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? nullableInt = 10;
+            double? nullableDouble = 3.14;
+            bool? nullableBool = null;
+            char? nullableChar = 'a';
+            int?[] arrayOfNullableInts = new int?[10];
+
+            DatabaseReader dr = new DatabaseReader();
+
+            int? i = dr.GetIntFromDatabase();
+
+            if (i.HasValue)
+                Console.WriteLine("Value of 'i' is: {0}", i.Value);
+            else
+                Console.WriteLine("Value of 'i' is undefined.");
+
+            bool? b = dr.GetBoolFromDatabase();
+
+            if (b != null)
+                Console.WriteLine("Value of 'b' is: {0}", b.Value);
+            else
+                Console.WriteLine("Value of 'b' is undefined.");
+        }
+
+    }
+    internal class DatabaseReader
+    {
+        public int? numericValue = null;
+        public bool? boolValue = true;
+
+        public int? GetIntFromDatabase() => numericValue;
+        public bool? GetBoolFromDatabase() => boolValue;
+    }
+}
