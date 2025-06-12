0a4d5335c2ef4cebd067a2d21b285a4995d51bb1
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-12 23:34:17 +0400

    Add Potion Effects (Not yet implemented)

--- CoreCSharp/PotionInventory/Potion.cs
+++ CoreCSharp/PotionInventory/Potion.cs
@@ -1,9 +1,19 @@
 ﻿namespace PotionInventory
 {
     enum PotionType { Healing, Mana, Stamina, Rehabilitating }
+    [Flags]
+    enum PotionEffect
+    {
+        None = 0,
+        Heal = 1,
+        Boost = 2,
+        Cure = 4,
+        Shield = 8
+    }
     struct Potion
     {
         public PotionType Type { get; set; }
+        public PotionEffect Effect { get; init; }
         public int RestoreAmount { get; set; }
 
         public Potion(PotionType type, int restoreAmount)
