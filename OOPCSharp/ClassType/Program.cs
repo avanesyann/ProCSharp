3b4f223cdf2f012b4a9a0a9da0a3e13710d54989
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-12 23:44:51 +0400

    Get ready for OOP

---
+++ OOPCSharp/ClassType/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Name = "BMW";
+        }
+    }
+    class Car
+    {
+        public string Name;
+        public int Speed;
+    }
+}
