f9336a0e565ccafc59ead728669dfe48a263a1a3
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-17 22:39:20 +0400

    Get familiar with access modifiers

---
+++ OOPCSharp/ClassType/Engine.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassType
+{
+    public class Engine
+    {
+        public bool oil;            // can be accessed from anywhere
+        private bool _crankshaft;   // can be accessed by the class
+        protected int _pistons;     // can be accessed by child classes
+        internal bool _camshaft;    // can be accessed within the current assembly
+    }
+}
