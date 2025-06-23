3893c7cd0b07e5f13b6192bf29026f7c6b286b40
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-23 23:48:26 +0400

    More on automatic properties

---
+++ OOPCSharp/AutoProps/Garage.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoProps
+{
+    class Garage
+    {
+        // The hidden int backing field is set to zero.
+        public int NumberOfCars { get; set; } = 1;
+
+        // The hidden Car backing field is set to null.
+        public Car? MyAuto { get; set; } = new Car();
+
+        public Garage()
+        {
+            // MyAuto = new Car();      // No need, since it's already initialized.
+            // NumberOfCars = 1;        // ...
+        }
+        public Garage(Car car, int numberOfCars)
+        {
+            MyAuto = car;
+            NumberOfCars = numberOfCars;
+        }
+    }
+}
--- OOPCSharp/AutoProps/Program.cs
+++ OOPCSharp/AutoProps/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Car c = new Car();
+            c.PetName = "Jerry";
+            c.Speed = 55;
+            c.Color = "Brown";
 
+            c.DisplayStats();
+
+            Console.WriteLine();
+
+            Garage g = new Garage();
+            g.MyAuto = c;
+            Console.WriteLine("Number of cars in garage: {0}", g.NumberOfCars);
+            Console.WriteLine("Your car is named: {0}", g.MyAuto.PetName);
         }
     }
 }
