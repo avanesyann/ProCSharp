823ec38ffc293e638502a74e01fda4e73cfa4adc
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-23 23:16:11 +0400

    Automatic Properties

---
+++ OOPCSharp/AutoProps/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoProps
+{
+    class Car
+    {
+        // Automatic properties! No need to define backing fields.
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+    }
+}
