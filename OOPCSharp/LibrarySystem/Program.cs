69ff646d3010858ffce43ddbe753b4c7d401fabb
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-26 23:49:23 +0400

    Add several new books

--- OOPCSharp/LibrarySystem/Program.cs
+++ OOPCSharp/LibrarySystem/Program.cs
@@ -8,6 +8,9 @@
             library.AddBook(new Book("War and Peace", "Leo Tolstoy", 1225));
             library.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", 336));
             library.AddBook(new Book("Flowers for Algernon", "Daniel Keyes", 311));
+            library.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", 180));
+            library.AddBook(new Book("Lolita", "Vladimir Nabokov", 336));
+            library.AddBook(new Book("The Trial", "Franz Kafka", 336));
 
             library.DisplayStats();
 
