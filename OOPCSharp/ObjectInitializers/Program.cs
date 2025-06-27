8985c33186b5a057ff2adb6e7bcc6177f208242e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-27 23:56:20 +0400

    Make some points

--- OOPCSharp/ObjectInitializers/Program.cs
+++ OOPCSharp/ObjectInitializers/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            // ...
+            // set each property manually
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // via a custom constructor
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            // object init syntax
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
         }
     }
 }
