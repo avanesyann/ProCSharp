2ba4add499866515173e98f6ffcb6115c5a460b9
Narek Avanesyan <avanesyan12345@gmail.com>
2025-06-28 17:00:39 +0400

    ReadOnly After Creation

--- OOPCSharp/ObjectInitializers/Program.cs
+++ OOPCSharp/ObjectInitializers/Program.cs
@@ -17,6 +17,17 @@
             // object init syntax
             Point finalPoint = new Point { X = 30, Y = 30 };
             finalPoint.DisplayStats();
+
+            Console.WriteLine();
+
+            // Make readonly point after construction
+            ReadOnlyPointAfterCreation firstReadonlyPoint = new ReadOnlyPointAfterCreation(20, 20);
+            firstReadonlyPoint.DisplayStats();
+
+            ReadOnlyPointAfterCreation secondReadonlyPoint = new ReadOnlyPointAfterCreation { X = 30, Y = 30 };
+            secondReadonlyPoint.DisplayStats();
+
+            Console.WriteLine();
         }
     }
 }
---
+++ OOPCSharp/ObjectInitializers/ReadOnlyPointAfterCreation.cs
@@ -0,0 +1,19 @@
+﻿namespace ObjectInitializers
+{
+    class ReadOnlyPointAfterCreation
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("InitOnlySetter: [{0}, {1}]", X, Y);
+        }
+        public ReadOnlyPointAfterCreation(int xVal, int yVal)
+        {
+            X = xVal;
+            Y = yVal;
+        }
+        public ReadOnlyPointAfterCreation() { }
+    }
+}
