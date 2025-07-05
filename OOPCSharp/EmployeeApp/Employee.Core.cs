127bf6c3485d12236e8d5e4e5d4f019db91e2647
Narek Avanesyan <avanesyan12345@gmail.com>
2025-07-05 14:22:29 +0400

    Partial Classes

---
+++ OOPCSharp/EmployeeApp/Employee.Core.cs
@@ -0,0 +1,61 @@
+﻿namespace EmployeeApp
+{
+    partial class Employee
+    {
+        // Members of a class that represent an object's state should not be marked as public.
+
+        private string _empName;                // backing field
+        private int _empId;                     // backing field
+        private float _currPay;                 // ...
+        private int _empAge;                    // ..
+        private string _empSSN;
+        private EmployeePayTypeEnum _payType;
+        private DateTime _hireDate;
+
+        public string Name
+        {
+            get { return _empName; }
+            set
+            {
+                if (value.Length > 15)      // "value" is a contextual keyword, not a true C# keyword
+                {
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                }
+                else
+                {
+                    _empName = value;
+                }
+            }
+        }
+        public int Id
+        {
+            get => _empId;
+            set => _empId = value;
+        }
+        public float Pay
+        {
+            get { return _currPay; }
+            set { _currPay = value; }
+        }
+        public int Age
+        {
+            get { return _empAge; }
+            set { _empAge = value; }
+        }
+        public string SocialSecurityNumber
+        {
+            get { return _empSSN; }
+            private set { _empSSN = value; }
+        }
+        public EmployeePayTypeEnum PayType
+        {
+            get => _payType;
+            set => _payType = value;
+        }
+        public DateTime HireDate
+        {
+            get => _hireDate;
+            set => _hireDate = value;
+        }
+    }
+}
--- OOPCSharp/EmployeeApp/Employee.cs
+++ OOPCSharp/EmployeeApp/Employee.cs
@@ -1,17 +1,7 @@
 ﻿namespace EmployeeApp
 {
-    class Employee
+    partial class Employee
     {
-        // Members of a class that represent an object's state should not be marked as public.
-
-        private string _empName;                // backing field
-        private int _empId;                     // backing field
-        private float _currPay;                 // ...
-        private int _empAge;                    // ..
-        private string _empSSN;
-        private EmployeePayTypeEnum _payType;
-        private DateTime _hireDate;
-
         public Employee() { }
         public Employee(string name, int id, float pay, string empSsn) : this(name, 0, id, pay, empSsn, EmployeePayTypeEnum.Salaried) { }
         public Employee(string name, int age, int id, float pay, string empSsn, EmployeePayTypeEnum payType)
@@ -22,52 +12,6 @@
             Pay = pay;
             SocialSecurityNumber = empSsn;
             PayType = payType;
-        }
-
-        public string Name
-        {
-            get { return _empName; }
-            set
-            {
-                if (value.Length > 15)      // "value" is a contextual keyword, not a true C# keyword
-                {
-                    Console.WriteLine("Error! Name length exceeds 15 characters!");
-                }
-                else
-                {
-                    _empName = value;
-                }
-            }
-        }
-        public int Id
-        {
-            get => _empId;
-            set => _empId = value;
-        }
-        public float Pay
-        {
-            get { return _currPay; }
-            set { _currPay = value; }
-        }
-        public int Age
-        {
-            get { return _empAge; }
-            set { _empAge = value; }
-        }
-        public string SocialSecurityNumber
-        {
-            get { return _empSSN; }
-            private set { _empSSN = value; }
-        }
-        public EmployeePayTypeEnum PayType
-        {
-            get => _payType;
-            set => _payType = value;
-        }
-        public DateTime HireDate
-        {
-            get => _hireDate;
-            set => _hireDate = value;
         }
 
         public void GiveBonus(float amount)
