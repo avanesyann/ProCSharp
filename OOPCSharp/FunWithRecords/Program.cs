bee14c193373199068a1530c846506a6f086903e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-07-08 22:53:17 +0400

    Deconstructing records

--- OOPCSharp/FunWithRecords/Program.cs
+++ OOPCSharp/FunWithRecords/Program.cs
@@ -26,7 +26,12 @@ Console.WriteLine("*** Records ***");
 //Console.WriteLine();
 
 CarRecord bmw = new CarRecord("BMW", "M4", "Dark Gray");
-Console.WriteLine(bmw.Make);
-Console.WriteLine(bmw.Model);
-Console.WriteLine(bmw.Color);
+bmw.Deconstruct(out string make, out string model, out string color);
+Console.WriteLine("Make: {0}, Model: {1}, Color: {2}", bmw.Make, bmw.Model, bmw.Color);
+Console.WriteLine("Deconstructed - Make: {0}, Model: {1}, Color: {2}", make, model, color);
 Console.WriteLine();
+
+// Tuples syntax
+var (make2, model2, color2) = bmw;
+//(string make2, string model2, string color2) = bmw;
+Console.WriteLine("Deconstructed - Make: {0}, Model: {1}, Color: {2}", make2, model2, color2);
