afff6ac79672f7ff5ac7cb0eaf4e25edba17abc2
Narek Avanesyan <avanesyan12345@gmail.com>
2025-07-14 20:40:56 +0400

    Smart Thermostat

--- OOPCSharp/SmartHome/Program.cs
+++ OOPCSharp/SmartHome/Program.cs
@@ -1,11 +1,18 @@
 ﻿using SmartHome;
 
 Home myHome = new Home();
-SmartDevice smartBulb = new SmartDevice { Name = "Smart Bulb"};
+SmartDevice smartBulb = new SmartBulb { Name = "Smart Bulb"};
 
 myHome.AddDevice(smartBulb);
 myHome.AddDevice("Thermostat");
 myHome.AddDevice("Speaker").TogglePower();
 
 smartBulb.TogglePower();
+
+var thermostat = new SmartThermostat("Living Room Thermostat", 22.5);
+myHome.AddDevice(thermostat);
+
+thermostat.SetTemperature(18);
+thermostat.TogglePower();
+
 myHome.ShowStatus();
--- OOPCSharp/SmartHome/SmartDevice.cs
+++ OOPCSharp/SmartHome/SmartDevice.cs
@@ -10,4 +10,27 @@
         public SmartDevice() { }
         public SmartDevice(string name) => Name = name;
     }
+
+    class SmartBulb : SmartDevice
+    {
+        public int Brightness { get; set; }
+        public string Color { get; set; }
+    }
+
+    class SmartThermostat : SmartDevice
+    {
+        public double Temperature { get; set; }
+
+        public SmartThermostat(string name, double initialTemp)
+        {
+            Name = name;
+            Temperature = initialTemp;
+        }
+
+        public void SetTemperature(double newTemp)
+        {
+            Temperature = newTemp;
+            Console.WriteLine($"{Name} temperature set to {Temperature}°C");
+        }
+    }
 }
