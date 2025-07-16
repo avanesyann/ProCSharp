84b9554873f92c16836d2b64f0ab21e391f799eb
Narek Avanesyan <avanesyan12345@gmail.com>
2025-07-16 18:26:10 +0400

    Give Bonus

--- OOPCSharp/Employees/Program.cs
+++ OOPCSharp/Employees/Program.cs
@@ -14,3 +14,18 @@ double cost = chucky.GetBenefitCost();
 Console.WriteLine($"Benefit Cost: {cost}");
 
 Employee.BenefitPackage.BenefitPackageLevel myBenefitLevel = Employee.BenefitPackage.BenefitPackageLevel.Platinum;
+
+
+// The publicly inherited GiveBonus() method operates identically for all subclasses.
+// Ideally, the bonus of a salesperson should consider the number of sales.
+chucky.GiveBonus(300);
+chucky.DisplayStats();
+Console.WriteLine();
+
+SalesPerson fran = new SalesPerson("Fran", 43, 93, 3000, "932-32-3232", 31);
+fran.GiveBonus(200);
+fran.DisplayStats();
+Console.WriteLine();
+
+// For this reason, using the virtual and override keywords comes handy.
+// ...
