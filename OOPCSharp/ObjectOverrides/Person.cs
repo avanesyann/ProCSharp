c4397a98d72ca70f9d5d9de70afbd676b7953121
Narek Avanesyan <avanesyan12345@gmail.com>
2025-07-29 10:22:33 +0400

    Object Overrides

---
+++ OOPCSharp/ObjectOverrides/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace ObjectOverrides
+{
+    // Remember! Person extends object.
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+        public Person() { }
+    }
+}
---
+++ OOPCSharp/ObjectOverrides/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ObjectOverrides
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+
+            // Use inherited members of System.Object.
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("Hash Code: {0}", p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+
+            Person p2 = p1;
+            object o = p2;
+
+            // Are the references pointing to the same object in memory?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+        }
+    }
+}
