103e03097893839dd3f92c239e8997133a7750eb
Narek Avanesyan <avanesyan12345@gmail.com>
2025-08-03 20:37:02 +0400

    Add Bicycle

---
+++ OOPCSharp/VehicleRental/Bicycle.cs
@@ -0,0 +1,18 @@
+﻿namespace VehicleRental
+{
+    public class Bicycle : Vehicle
+    {
+        public bool IsElectric { get; set; }
+
+        public override double CalculateRentalPrice(int hours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Is {(IsElectric ? "Electric" : "Not Electric")}");
+        }
+    }
+}
