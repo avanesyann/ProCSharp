62bee3c0b4688bce65ab4bfeb09dc2a4095a79bc
Narek Avanesyan <avanesyan12345@gmail.com>
2025-08-08 22:59:34 +0400

    Add Dragons

--- OOPCSharp/MagicalZoo/MagicalCreature.cs
+++ OOPCSharp/MagicalZoo/MagicalCreature.cs
@@ -5,4 +5,18 @@
         public string Name { get; set; }
         public abstract void MakeSound();
     }
+
+    public class Dragon : MagicalCreature
+    {
+        public bool CanBreatheFire { get; set; }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Roarrr!");
+        }
+        public void BreatheFire()
+        {
+            Console.WriteLine("Woosh!");
+        }
+    }
 }
--- OOPCSharp/MagicalZoo/Program.cs
+++ OOPCSharp/MagicalZoo/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            MagicalCreature creature = new Dragon();
+            creature.MakeSound();
 
+            if (creature is Dragon)
+            {
+                ((Dragon)creature).BreatheFire();
+            }
         }
     }
 }
