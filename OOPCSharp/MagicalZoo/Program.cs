37c370b1fe4e63994e5d40240506d9370af4fca7
Narek Avanesyan <avanesyan12345@gmail.com>
2025-08-10 23:15:20 +0400

    Static method CreatureInfo

--- OOPCSharp/MagicalZoo/Program.cs
+++ OOPCSharp/MagicalZoo/Program.cs
@@ -12,6 +12,7 @@
             foreach (MagicalCreature magicalCreature in magicalCreatures)
             {
                 magicalCreature.MakeSound();
+                // PrintCreatureInfo(magicalCreature);
 
                 if (magicalCreature is Dragon dragon)
                 {
@@ -26,6 +27,33 @@
                     ((Unicorn)magicalCreature).Heal();
                 }
                 Console.WriteLine();
+            }
+        }
+        public static void PrintCreatureInfo(MagicalCreature creature)
+        {
+            switch (creature)
+            {
+                case Dragon d:
+                    Console.WriteLine($"Creature: Dragon - Name: {d.Name}");
+                    d.MakeSound();
+                    d.BreatheFire();
+                    break;
+                case Phoenix p:
+                    Console.WriteLine($"Creature: Phoenix - Name: {p.Name}");
+                    p.MakeSound();
+                    p.Revive();
+                    break;
+                case Unicorn u:
+                    Console.WriteLine($"Creature: Dragon - Name: {u.Name}");
+                    u.MakeSound();
+                    u.Heal();
+                    break;
+                case null:
+                    Console.WriteLine("No creature provided.");
+                    break;
+                default:
+                    Console.WriteLine($"Unknown creature type: {creature.GetType().Name}");
+                    break;
             }
         }
     }
