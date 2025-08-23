0d105b1f7e29392dee28d19e10d5cd4661c87d9d
Narek Avanesyan <avanesyan12345@gmail.com>
2025-08-23 16:53:14 +0400

    Car class for exception handling

---
+++ OOPCSharp/SimpleException/Car.cs
@@ -0,0 +1,49 @@
+﻿namespace SimpleException
+{
+    public class Car
+    {
+        public const int MaxSpeed = 100;
+
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        private bool _carIsDead;
+
+        private readonly Radio _theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            _theMusicBox.TurnOn(state);
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (_carIsDead)
+            {
+                Console.WriteLine("{0} is out of order...", PetName);
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine("{0} has overheated!", PetName);
+                    CurrentSpeed = 0;
+                    _carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ OOPCSharp/SimpleException/Program.cs
@@ -0,0 +1,11 @@
+﻿using SimpleException;
+
+Console.WriteLine("***** Simple Exception Example *****");
+Console.WriteLine("=> Creating a car and stepping on it!");
+Car myCar = new Car("Zippy", 20);
+myCar.CrankTunes(true);
+
+for (int i = 0; i < 10; i++)
+{
+    myCar.Accelerate(10);
+}
