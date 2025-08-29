da1a2a99851553757cc4f3e78cc9c4202da944fa
Narek Avanesyan <avanesyan12345@gmail.com>
2025-08-29 23:55:41 +0400

    Take 2 uncomment

--- OOPCSharp/CustomException/CarIdDeadException.cs
+++ OOPCSharp/CustomException/CarIdDeadException.cs
@@ -2,25 +2,25 @@
 {
     public class CarIsDeadException : ApplicationException
     {
-        private string _messageDetails = string.Empty;
+        //private string _messageDetails = string.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
-        public CarIsDeadException(string message, string cause, DateTime time)
-        {
-            _messageDetails = message;
-            CauseOfError = cause;
-            ErrorTimeStamp = time;
-        }
-
-        // you are not required to override the virtual Message property
-        //public CarIsDeadException(string message, string cause, DateTime time) : base(message)
+        //public CarIsDeadException(string message, string cause, DateTime time)
         //{
+        //    _messageDetails = message;
         //    CauseOfError = cause;
         //    ErrorTimeStamp = time;
         //}
 
-        public override string Message => $"Car Error Message: {_messageDetails}";
+        // you are not required to override the virtual Message property
+        public CarIsDeadException(string message, string cause, DateTime time) : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+
+        //public override string Message => $"Car Error Message: {_messageDetails}";
     }
 }
