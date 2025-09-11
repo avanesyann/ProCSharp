c8209cd21e340441871ef562ced31f8e69e526cd
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-11 20:13:31 +0400

    Working with Interfaces

---
+++ OOPCSharp/CustomInterfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("***** A First Look at Interfaces *****\n");
+CloneableExample();
+
+static void CloneableExample()
+{
+    // All of these classes support the ICloneable interface.
+    string myStr = "Hello";
+    OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+
+    // Therefore, they can all be passed into a method taking ICloneable.
+    CloneMe(myStr);
+    CloneMe(unixOS);
+
+    static void CloneMe(ICloneable c)
+    {
+        // Clone whatever we get and print out the name.
+        object theClone = c.Clone();
+        Console.WriteLine("Your clone is a: {0}", theClone.GetType().Name);
+    }
+}
---
+++ OOPCSharp/CustomInterfaces/README.md
@@ -0,0 +1,3 @@
+## Interfaces
+
+An interface is nothing more than a named set of *abstract members*. (abstract methods are pure protcol, they do not provide a default implementation)
