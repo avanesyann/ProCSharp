0c1fd91f496e9361d5e36a150daabab1a5278a28
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-13 23:56:00 +0400

    Implementing an interface

--- OOPCSharp/CustomInterfaces/IPointy.cs
+++ OOPCSharp/CustomInterfaces/IPointy.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace CustomInterfaces
 {
+    // This class derives from System.Object and implements a single interface.
+    // public class Pencil : IPointy { }
+
+    // This class also derives from System.Object and implements a single interface.
+    // public class SwitchBlade : Object, IPointy { }
+
+    // This class derives from a custom base class and implements a single inetrface.
+    //public class Fork : Utensil, IPointy { }
+
+    // This struct implicity derives from System.ValueType and implements two interfaces.
+    //public struct PitchFork : ICloneable, IPointy { }
+
     public interface IPointy
     {
         // Implicitly public and abstract.
