51e81c3be391247a9bac0c17ce722d9368bfcb1b
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-18 23:57:22 +0400

    IRegularPointy.cs + Square.cs

---
+++ OOPCSharp/CustomInterfaces/IRegularPointy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterfaces
+{
+    public interface IRegularPointy : IPointy
+    {
+        int SideLength { get; set; }
+        int NumberOfSides { get; set; }
+        int Perimeter => SideLength * NumberOfSides;
+    }
+}
