790640912ed2ab605ddebfa984bf35fff4208529
Narek Avanesyan <avanesyan12345@gmail.com>
2025-10-20 23:57:42 +0400

    Classes, Objects, and References

---
+++ OOPCSharp/SimpleGC/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleGC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create new car object on the managed heap.
+            // We are returned a reference to the object
+            Car refToMyCar = new Car("Zippy", 50);
+
+            // The C# dot operator is used to invoke members
+            // on the object using our reference variable.
+            Console.WriteLine(refToMyCar.ToString());
+        }
+    }
+}
